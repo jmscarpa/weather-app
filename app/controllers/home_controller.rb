4789d60ac73c17a8d9b659b574b702c6f70b64e9
João Mateus Scarpa <joao.scarpa@gmail.com>
2026-02-12 21:20:48 -0300

    First commit - rails setup

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def search
+    render :index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: "home#index"
+  post "/search", to: "home#search", as: :search
+end
